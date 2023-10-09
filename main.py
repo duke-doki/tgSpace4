d6e8e96b8c294a46cb4f0bd3212f72212bcb388e
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-10-09 22:14:13 +0300

    publish photo to Telegram channel

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ tg_token = env.str('TG_TOKEN')
 bot = telegram.Bot(token=tg_token)
 #print(bot.get_me())
 
-bot.send_message(text='Hi John!', chat_id='@tgspace4')
+#bot.send_message(text='Hi John!', chat_id='@tgspace4')
+
+bot.send_document(chat_id='@tgspace4', document=open('images/epic1.png', 'rb'))
