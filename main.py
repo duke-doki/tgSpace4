201b1a32dad426a49162b7635892f1b4496e9af3
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-10-09 22:10:32 +0300

    publish text to Telegram channel

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ env = Env()
 env.read_env()
 tg_token = env.str('TG_TOKEN')
 bot = telegram.Bot(token=tg_token)
-print(bot.get_me())
+#print(bot.get_me())
+
+bot.send_message(text='Hi John!', chat_id='@tgspace4')
