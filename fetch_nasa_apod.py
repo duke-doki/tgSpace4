cac6d2fcaef073d7132cb2d8e0cb9cab44cc908b
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-10-12 16:00:13 +0300

    minor fixes

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -6,21 +6,20 @@ from environs import Env
 from download_image import download_image, fetch_file_extension
 
 
-def fetch_nasa_apod(token, url):
+def fetch_nasa_apod(token):
+    url = 'https://api.nasa.gov/planetary/apod'
     params = {'start_date': '2023-09-01', 'api_key': token}
     response = requests.get(url, params=params)
     response.raise_for_status()
-    apods = response.json()
-
-    for apod_number, apod in enumerate(apods, start=1):
-        download_image(apod['url'], f'images/nasa_apod{apod_number}')
+    return response.json()
 
 
 if __name__ == '__main__':
-    url = 'https://api.nasa.gov/planetary/apod'
     Path('images').mkdir(exist_ok=True)
     env = Env()
     env.read_env()
     nasa_token = env.str('NASA_TOKEN')
-    if fetch_file_extension(url):
-        fetch_nasa_apod(nasa_token, url)
+    apods = fetch_nasa_apod(nasa_token)
+    for apod_number, apod in enumerate(apods, start=1):
+        if fetch_file_extension(apod['url']):
+            download_image(apod['url'], f'images/nasa_apod{apod_number}')
